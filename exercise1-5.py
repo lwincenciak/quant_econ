acd213c9ff75a811ace3a34af8be5955cfd3370b
Leszek Wincenciak <leszekflickr@gmail.com>
2018-12-05 15:54:13 +0100

    5/12/2018

--- exercise1-5.py
+++ exercise1-5.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 
 ts_length = 200
 # x_values = [0]  # empty list
-a_list = [0, 0.8, 0.98, 1.02]
+a_list = [0, 0.95, 0.98]
+colors = ['#0055DD', '#009933', '#BB0022']
 
 
 def tseries(a):
@@ -16,9 +17,22 @@ def tseries(a):
     return x_values
 
 
+fig = plt.figure(figsize=(12, 6))
+plt.xlim(0, ts_length)
+# plt.ylim(3.35, 3.65)
+
+plt.rcParams.update({'font.size': 14})
+
+i = 0
 for a in a_list:
     x = tseries(a)
-    plt.plot(x, label='a = ' + str(a))
+    plt.plot(x, linewidth=1.2, color=colors[i], label='$\\alpha = $' + str(a))
+    i += 1
 
+plt.grid(True, 'major', 'both', ls='--', lw=.5, c='k', alpha=.3)
+plt.xlabel('Time [t]')
+plt.ylabel('')
+plt.title('Time series for different autoregressive parameter\n$x_t = \\alpha x_{t-1} + \\varepsilon_t$')
 plt.legend()
 plt.show()
+fig.savefig("tseries.pdf", bbox_inches='tight')
